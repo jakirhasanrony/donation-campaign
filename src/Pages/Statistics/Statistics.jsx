74c2402ff72fc74075d24a2d269e00b9fc32458c
Md. Jakir Hasan Rony <jakir15-14584@diu.edu.bd>
2023-09-27 00:12:18 +0600

    Statistics page design added

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,32 +1,11 @@
-import { useEffect, useState } from "react";
-import { useLoaderData } from "react-router-dom";
-import { PieChart, Pie, Cell, Legend, Tooltip, Label, LabelList  } from 'recharts';
-
-const CustomPieLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, name }) => {
-    const radius = innerRadius + (outerRadius - innerRadius) * 0.8;
-    const x = cx + radius * Math.cos(-midAngle * (Math.PI / 180));
-    const y = cy + radius * Math.sin(-midAngle * (Math.PI / 180));
-
-    return (
-        <text
-            x={x}
-            y={y}
-            fill="black"
-            fontSize={14}
-            textAnchor={x > cx ? "start" : "end"}
-            dominantBaseline="central"
-        >
-            {name === "Donation Item" ? `${(percent * 100).toFixed(0)}%` : null}
-        </text>
-    );
-};
+import React, { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { PieChart, Pie, Cell } from 'recharts';
 
 const Statistics = () => {
     const cards = useLoaderData();
     const [donatedItem, setDonatedItem] = useState([]);
     const [notFound, setNotFound] = useState(false);
-
-
 
 
     useEffect(() => {
@@ -40,61 +19,63 @@ const Statistics = () => {
         }
 
     }, []);
+
     const totalCardsCount = cards.length;
     const donatedCardsCount = donatedItem.length;
     const remainingCardsCount = totalCardsCount - donatedCardsCount;
-
-    const donatedCardsPercentage = (donatedCardsCount / totalCardsCount) * 100;
-    const remainingCardsPercentage = (remainingCardsCount / totalCardsCount) * 100;
-
-
-
-    const pieChartData = [
-        { name: "Your Donation", value: donatedCardsPercentage },
-        { name: "Remaining Cards", value: remainingCardsPercentage },
+    const data = [
+        { name: "Your Donation", value: donatedCardsCount },
+        { name: "Remaining Cards", value: remainingCardsCount },
     ];
 
+    const COLORS = ['#FF444A', '#00C49F'];
 
-    const COLORS = ["#00C49F", "#FF8042"];
+    const RADIAN = Math.PI / 180;
 
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
+        return (
+            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+                {`${(percent * 100).toFixed(0)}%`}
+            </text>
+        );
+    };
 
     return (
         <div>
-
-            statistic page
-            <div>Total Cards: {totalCardsCount}</div>
-            <div>Donated Cards: {donatedCardsCount}</div>
-            <div>Remaining Cards: {remainingCardsCount}</div>
-
-            <PieChart width={400} height={400}>
-                <Pie
-                    data={pieChartData}
-                    cx={200}
-                    cy={200}
-                    startAngle={0}
-                    endAngle={360}
-                    innerRadius={0} // Set innerRadius to 0
-                    outerRadius={80}
-                    fill="#8884d8"
-                    paddingAngle={5}
-                    dataKey="value"
-                    labelLine={false} // Disable label lines
-                >
-                    {pieChartData.map((entry, index) => (
-                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                    ))}
-                    <LabelList
-                        dataKey="name"
-                        position="outside"
-                        content={(props) => <CustomPieLabel {...props} />}
-                    />
-                </Pie>
-                <Tooltip />
-                <Legend formatter={(value, entry) => entry.name === "Donation Item" ? `${(entry.payload.value).toFixed(0)}%` : value} />
-            </PieChart>
+            <div className='flex justify-center items-center mb-16 mt-16'>
+                <PieChart width={400} height={400}>
+                    <Pie
+                        data={data}
+                        cx="50%"
+                        cy="50%"
+                        labelLine={false}
+                        label={renderCustomizedLabel}
+                        outerRadius={180}
+                        fill="#8884d8"
+                        dataKey="value"
+                    >
+                        {data.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                        ))}
+                    </Pie>
+                </PieChart>
+            </div>
+            <div className='flex items-center justify-center gap-14'>
+                <div className='flex items-center justify-center gap-4'>
+                    <h2 className='font-medium'>Your Donation</h2>
+                    <div className='w-28 h-3 bg-[#00C49F] rounded'></div>
+                </div>
+                <div className='flex items-center justify-center gap-4'>
+                    <h2 className='font-medium'>Total Donation</h2>
+                    <div className='w-28 h-3 bg-[#FF444A] rounded'></div>
+                </div>
+            </div>
         </div>
     );
-};
+}
 
 export default Statistics;
